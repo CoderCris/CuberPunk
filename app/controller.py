1af438168105a67e2a522592306ff0fe95d92041
CoderCris <crisamer.ortega@gmail.com>
2024-07-16 22:42:59 +0000

    updating DB

--- app/controller.py
+++ app/controller.py
@@ -1,4 +1,14 @@
 import db as db
 
+
+def get_characters():
+    return db.characters
+
+
+#make it finer
+def get_character(character_id: int):
+    character = next((char for char in db.characters if char.id == character_id), None)
+    return character
+
 def skill_list():
     return "Skill List"
--- app/db.py
+++ app/db.py
@@ -1,7 +1,27 @@
-# db.py
-from typing import List
-from models import Character, Skill, Role
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models import Base, User
 
-characters: List[Character] = []
-skills: List[Skill] = []
-roles: List[Role] = []
+# Create an engine that stores data in the local directory's sqlite database file
+engine = create_engine('sqlite:///mydatabase.db' )
+
+# Create all tables in the engine
+Base.metadata.create_all(engine)
+
+# Create a configured "Session" class
+Session = sessionmaker(bind=engine)
+
+# Create a Session
+session = Session()
+
+# Create a new user and add it to the session
+new_user = User(name="John Doe", email="john.doe@example.com")
+session.add(new_user)
+
+# Commit the session to write the data to the database
+session.commit()
+
+# Query the database
+users = session.query(User).all()
+for user in users:
+    print(user.name, user.email)
