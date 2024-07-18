2cdc52d71d1ba5bef773fa40213a7bc8e770abd3
CoderCris <crisamer.ortega@gmail.com>
2024-07-18 17:38:24 +0000

    setting up DB conn

--- app/controller.py
+++ app/controller.py
@@ -10,5 +10,32 @@ def get_character(character_id: int):
     character = next((char for char in db.characters if char.id == character_id), None)
     return character
 
+def create_character(character):
+    db.characters.append(character)
+    return character
+
+#make it finer
+def update_character(character_id: int, updated_character):
+    character = next((char for char in db.characters if char.id == character_id), None)
+    if character is None:
+        return None
+
+    character.name = updated_character.name
+    character.role_id = updated_character.role_id
+    character.skill_ids = updated_character.skill_ids
+    character.equipment = updated_character.equipment
+    return character
+
+def delete_character(character_id: int):
+    character = next((char for char in db.characters if char.id == character_id), None)
+    if character is None:
+        return None
+
+    db.characters.remove(character)
+    return character
+
+def search_character_by_name(name: str):
+    return [char for char in db.characters if char.name == name]
+
 def skill_list():
     return "Skill List"
--- app/models.py
+++ app/models.py
@@ -1,17 +1,35 @@
 from pydantic import BaseModel
-from typing import List, Optional
+from sqlalchemy import ForeignKey, Integer, String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+
+
+
+class Base(DeclarativeBase):
+    pass
+
+class User(BaseModel):
+    _tablename_ = 'users'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(30))
+    address: Mapped[str] = mapped_column(String(70))
 
 class Skill(BaseModel):
-    id: int
-    name: str
+    _tablename_ = 'skills'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(30))
 
 class Role(BaseModel):
-    id: int
-    name: str
+    _tablename_ = 'roles'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(30))
 
 class Character(BaseModel):
-    id: int
-    name: str
-    role_id: int
-    skill_ids: List[int]
-    #equipment: Optional[List[str]] = []
+    _tablename_ = 'characters'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(30))
+    role_id: Mapped[int] = mapped_column(Integer, ForeignKey('roles.id'))
+    skill_ids: Mapped[List[int]] = mapped_column(List[int], ForeignKey('skills.id'))
+
